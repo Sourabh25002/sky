18fb8aae3387d0dbfabd7074643bb7d77626a696
Sourabh <sk25002kumar@gmail.com>
2025-11-18 23:52:50 +0530

    Setting up the backend

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+const app = express();
+
+// Default route
+app.get("/", (req, res) => {
+    res.send("Hello, sky");
+});
+
+// PORT setup and server start
+const PORT = process.env.PORT;
+if (!PORT) {
+    console.error("Error: PORT environment variable is not defined.");
+    process.exit(1); 
+}
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT:${PORT}`);
+});
